f8bd0b826b96d756ec9a691bc907fde3444e5aea
chizakasipascal <pascalkasichiza@gmail.com>
2022-05-30 12:11:15 +0200

    register ui correction et adjustement

--- lib/src/views/screens/register.dart
+++ lib/src/views/screens/register.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mobileappclientv1/src/const/app_theme.dart';
 import 'package:mobileappclientv1/src/const/routes.dart';
 import 'package:mobileappclientv1/src/utils/utils.dart';
 import 'package:mobileappclientv1/src/views/widgets/widgets.dart';
@@ -22,218 +23,199 @@ class _RegisterState extends State<Register> {
 
   bool value = false;
 
+  bool m = false;
+  bool f = false;
+
   @override
   Widget build(BuildContext context) {
-    // final l10n = AppLocalizations.of(context);
     final isKeyboard = MediaQuery.of(context).viewInsets.bottom != 0;
-
-    //TODO : Add the Form Widget to verify all controller and validator form
+    final size = MediaQuery.of(context).size;
     return Stack(
       children: [
         BuildBackground(
-          viewLogo: !isKeyboard ? true : false,
+          viewLogo: !isKeyboard ? false : false,
         ),
         Scaffold(
           backgroundColor: Colors.transparent,
-          body: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              TextFieldCustomer(
-                // hint: "Prénom", //l10n!.nom,
-                label: "Prénom *",
-                textAlign: TextAlign.left,
-                prefixIcon: const Padding(
-                  padding: EdgeInsets.only(
-                      top: 20.0, left: 20.0, right: 15.0, bottom: 0),
-                  child: SizedBox(
-                    height: 10,
-                    width: 10,
-                  ),
-                ),
-              ),
-              TextFieldCustomer(
-                // hint: "Prénom", //l10n!.nom,
-                label: "Nom *",
-                textAlign: TextAlign.left,
-                prefixIcon: const Padding(
-                  padding: EdgeInsets.only(
-                      top: 20.0, left: 20.0, right: 15.0, bottom: 0),
-                  child: SizedBox(
-                    height: 10,
-                    width: 10,
-                  ),
-                ),
-              ),
-              TextFieldCustomer(
-                // hint: "Prénom", //l10n!.nom,
-                label: "Téléphone *",
-                textAlign: TextAlign.left,
-                prefixIcon: const Padding(
-                  padding: EdgeInsets.only(
-                      top: 20.0, left: 20.0, right: 15.0, bottom: 0),
-                  child: SizedBox(
-                    height: 10,
-                    width: 10,
-                  ),
-                ),
-              ),
-              TextFieldCustomer(
-                // hint: "Prénom", //l10n!.nom,
-                label: "Email",
-                textAlign: TextAlign.left,
-                prefixIcon: const Padding(
-                  padding: EdgeInsets.only(
-                      top: 20.0, left: 20.0, right: 15.0, bottom: 0),
-                  child: SizedBox(
-                    height: 10,
-                    width: 10,
-                  ),
-                ),
-              ),
-              TextFieldCustomer(
-                // hint: "Prénom", //l10n!.nom,
-                label: "30/09/1932",
-                textAlign: TextAlign.left,
-                prefixIcon: const Padding(
-                  padding: EdgeInsets.only(
-                      top: 20.0, left: 20.0, right: 15.0, bottom: 0),
-                  child: SizedBox(
-                    height: 10,
-                    width: 10,
-                  ),
-                ),
-              ),
-              const Text("Sexe"),
-              TextFieldCustomer(
-                // hint: "Prénom", //l10n!.nom,
-                label: "Mot de passe *",
-                textAlign: TextAlign.left,
-                prefixIcon: const Padding(
-                  padding: EdgeInsets.only(
-                      top: 20.0, left: 20.0, right: 15.0, bottom: 0),
-                  child: SizedBox(
-                    height: 10,
-                    width: 10,
-                  ),
-                ),
-              ),
-              TextFieldCustomer(
-                // hint: "Prénom", //l10n!.nom,
-                label: "Confirmation du mot de passe *",
-                textAlign: TextAlign.left,
-                prefixIcon: const Padding(
-                  padding: EdgeInsets.only(
-                      top: 20.0, left: 20.0, right: 15.0, bottom: 0),
-                  child: SizedBox(
-                    height: 10,
-                    width: 10,
-                  ),
-                ),
-              ),
-              const SizedBox(height: 10),
-              ConnectisRedButtom(
-                onPressed: () {
-                  Navigator.pushNamed(context, Routes.page);
-                },
-                descriprion: "Se Connecter", //l10n.seconnecter,
-                style: Theme.of(context).elevatedButtonTheme.style!.copyWith(
-                      foregroundColor: MaterialStateProperty.all(kWhiteColor),
-                      overlayColor: MaterialStateProperty.all(
-                        kGreyColor.withOpacity(.5),
-                      ),
+          body: SingleChildScrollView(
+            physics: const BouncingScrollPhysics(),
+            child: Column(
+              children: [
+                TextFieldCustomer(
+                  label: "Prénom *",
+                  textAlign: TextAlign.left,
+                  prefixIcon: const Padding(
+                    padding: EdgeInsets.only(
+                        top: 20.0, left: 20.0, right: 15.0, bottom: 0),
+                    child: SizedBox(
+                      height: 10,
+                      width: 10,
                     ),
-              ),
-            ],
+                  ),
+                ),
+                TextFieldCustomer(
+                  label: "Nom *",
+                  textAlign: TextAlign.left,
+                  prefixIcon: const Padding(
+                    padding: EdgeInsets.only(
+                        top: 20.0, left: 20.0, right: 15.0, bottom: 0),
+                    child: SizedBox(
+                      height: 10,
+                      width: 10,
+                    ),
+                  ),
+                ),
+                TextFieldCustomer(
+                  label: "Téléphone *",
+                  textAlign: TextAlign.left,
+                  prefixIcon: const Padding(
+                    padding: EdgeInsets.only(
+                        top: 20.0, left: 20.0, right: 15.0, bottom: 0),
+                    child: SizedBox(
+                      height: 10,
+                      width: 10,
+                    ),
+                  ),
+                ),
+                TextFieldCustomer(
+                  label: "Email",
+                  textAlign: TextAlign.left,
+                  prefixIcon: const Padding(
+                    padding: EdgeInsets.only(
+                        top: 20.0, left: 20.0, right: 15.0, bottom: 0),
+                    child: SizedBox(
+                      height: 10,
+                      width: 10,
+                    ),
+                  ),
+                ),
+                TextFieldCustomer(
+                  label: "30/09/1932",
+                  textAlign: TextAlign.left,
+                  prefixIcon: const Padding(
+                    padding: EdgeInsets.only(
+                        top: 20.0, left: 20.0, right: 15.0, bottom: 0),
+                    child: SizedBox(
+                      height: 10,
+                      width: 10,
+                    ),
+                  ),
+                ),
+                Align(
+                  alignment: Alignment.bottomLeft,
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 50.0, top: 5),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Sexe",
+                          style: themeData.textTheme.bodyText2!
+                              .copyWith(fontSize: 12),
+                        ),
+                        Row(
+                          children: [
+                            Text(
+                              "M",
+                              style: themeData.textTheme.bodyText2!
+                                  .copyWith(fontSize: 16),
+                            ),
+                            Checkbox(
+                              fillColor:
+                                  MaterialStateProperty.all(Colors.transparent),
+                              side:
+                                  MaterialStateBorderSide.resolveWith((states) {
+                                if (states.contains(MaterialState.pressed)) {
+                                  return const BorderSide(color: kConnectis);
+                                } else {
+                                  return const BorderSide(color: kGreyColor);
+                                }
+                              }),
+                              checkColor: kConnectis,
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(5)),
+                              // value: snapshot.tapval,
+                              onChanged: (val) {
+                                //   snapshot.changval = val!;
+                              },
+                              value: true,
+                            ),
+                            Text(
+                              "F",
+                              style: themeData.textTheme.bodyText2!
+                                  .copyWith(fontSize: 16),
+                            ),
+                            Checkbox(
+                              fillColor:
+                                  MaterialStateProperty.all(Colors.transparent),
+                              side:
+                                  MaterialStateBorderSide.resolveWith((states) {
+                                if (states.contains(MaterialState.pressed)) {
+                                  return const BorderSide(color: kConnectis);
+                                } else {
+                                  return const BorderSide(color: kGreyColor);
+                                }
+                              }),
+                              checkColor: kConnectis,
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(5)),
+                              // value: snapshot.tapval,
+                              onChanged: (val) {
+                                //   snapshot.changval = val!;
+                              },
+                              value: true,
+                            ),
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+                LineWithCircleRoundConer(isEmptyOne: true, size: size),
+                TextFieldCustomer(
+                  label: "Mot de passe *",
+                  isPassword: true,
+                  textAlign: TextAlign.left,
+                  prefixIcon: const Padding(
+                    padding: EdgeInsets.only(
+                        top: 20.0, left: 20.0, right: 15.0, bottom: 0),
+                    child: SizedBox(
+                      height: 10,
+                      width: 10,
+                    ),
+                  ),
+                ),
+                TextFieldCustomer(
+                  label: "Confirmation du mot de passe *",
+                  isPassword: true,
+                  textAlign: TextAlign.left,
+                  prefixIcon: const Padding(
+                    padding: EdgeInsets.only(
+                        top: 20.0, left: 20.0, right: 15.0, bottom: 0),
+                    child: SizedBox(
+                      height: 10,
+                      width: 10,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 10),
+                ConnectisRedButtom(
+                  onPressed: () {
+                    Navigator.pushNamed(context, Routes.page);
+                  },
+                  descriprion: "Se Connecter",
+                  style: Theme.of(context).elevatedButtonTheme.style!.copyWith(
+                        foregroundColor: MaterialStateProperty.all(kWhiteColor),
+                        overlayColor: MaterialStateProperty.all(
+                          kGreyColor.withOpacity(.5),
+                        ),
+                      ),
+                ),
+              ],
+            ),
           ),
         ),
       ],
     );
   }
 }
-
-/*
-import 'package:flutter_vpn/flutter_vpn.dart';
-import 'package:flutter_vpn/state.dart';
-
-class MyApp extends StatefulWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  final _addressController = TextEditingController();
-  final _usernameController = TextEditingController();
-  final _passwordController = TextEditingController();
-
-  var state = FlutterVpnState.disconnected;
-  CharonErrorState? charonState = CharonErrorState.NO_ERROR;
-
-  @override
-  void initState() {
-    FlutterVpn.prepare();
-    FlutterVpn.onStateChanged.listen((s) => setState(() => state = s));
-    super.initState();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Flutter VPN'),
-        ),
-        body: ListView(
-          padding: const EdgeInsets.all(12),
-          children: <Widget>[
-            Text('Current State: $state'),
-            Text('Current Charon State: $charonState'),
-            TextFormField(
-              controller: _addressController,
-              decoration: const InputDecoration(icon: Icon(Icons.map_outlined)),
-            ),
-            TextFormField(
-              controller: _usernameController,
-              decoration: const InputDecoration(
-                icon: Icon(Icons.person_outline),
-              ),
-            ),
-            TextFormField(
-              controller: _passwordController,
-              obscureText: true,
-              decoration: const InputDecoration(icon: Icon(Icons.lock_outline)),
-            ),
-            ElevatedButton(
-              child: const Text('Connect'),
-              onPressed: () => FlutterVpn.connectIkev2EAP(
-                server: _addressController.text,
-                username: _usernameController.text,
-                password: _passwordController.text,
-              ),
-            ),
-            ElevatedButton(
-              child: const Text('Disconnect'),
-              onPressed: () => FlutterVpn.disconnect(),
-            ),
-            ElevatedButton(
-              child: const Text('Update State'),
-              onPressed: () async {
-                var newState = await FlutterVpn.currentState;
-                setState(() => state = newState);
-              },
-            ),
-            ElevatedButton(
-              child: const Text('Update Charon State'),
-              onPressed: () async {
-                var newState = await FlutterVpn.charonErrorState;
-                setState(() => charonState = newState);
-              },
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-*/
